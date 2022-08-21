2c446b7d29798951fbc4ab9be4bf31ee30388f8d
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2022-08-21 16:03:59 +0200

    ci: suppress commitlint for release commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,4 +4,16 @@
  * @Copyright: Technology Studio
 **/
 
-module.exports = { extends: ['@commitlint/config-conventional'] }
+const automaticCommitPattern = /^chore\(release\):.*\[skip ci]/
+
+const commitlintConfig = {
+  extends: [
+    '@commitlint/config-conventional',
+  ],
+
+  ignores: [
+    commitMsg => automaticCommitPattern.test(commitMsg),
+  ],
+}
+
+module.exports = commitlintConfig
