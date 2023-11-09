8443e0b4c818d7742b8aee13a5038d304028c9ae
Erik Slovák <34162967+erik-slovak@users.noreply.github.com>
2023-11-09 19:57:48 +0100

    fix: add workaround to fix issues with `eslint-plugin-import` (#398)
    * fix: add workaround to fix issues with `eslint-plugin-import`
    
    * chore: add links for the issue

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,8 @@ const typescriptConfig = {
     parser: Parser,
     parserOptions: {
       project: './tsconfig.json',
+      ecmaVersion: 'latest', // NOTE: added to fix issue with eslint-plugin-import - https://github.com/import-js/eslint-plugin-import/issues/2556
+      sourceType: 'module', // NOTE: added to fix issue with eslint-plugin-import - https://github.com/import-js/eslint-plugin-import/issues/2556
     },
   },
   rules: {
@@ -40,8 +42,13 @@ const typescriptConfig = {
     ...eslintCommentsRules,
   },
   settings: {
+    // NOTE: added to fix issue with eslint-plugin-import - https://github.com/import-js/eslint-plugin-import/issues/2556
+    'import/parsers': {
+      espree: ['.js', '.cjs', '.mjs', '.jsx'],
+    },
     'import/resolver': {
       typescript: true,
+      node: true, // NOTE: added to fix issue with eslint-plugin-import - https://github.com/import-js/eslint-plugin-import/issues/2556
     },
   },
 }
