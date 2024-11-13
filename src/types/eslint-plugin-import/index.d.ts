e93da0a33744b8a5e44b089d486db2a7913d3e70
Erik Slovak <erik.slovak@technologystudio.sk>
2024-11-13 09:41:52 +0100

    fix: move declarations into single file

--- src/types/eslint-plugin-import/index.d.ts
+++ src/types/eslint-plugin-import/index.d.ts
@@ -1,11 +1,18 @@
-import type {
-  ESLint, Linter,
-} from 'eslint'
-
 declare module 'eslint-plugin-import' {
+  import type {
+    ESLint, Linter,
+  } from 'eslint'
   export const flatConfigs: {
     recommended: Linter.Config
     typescript: Linter.Config
   }
   export const rules: NonNullable<ESLint.Plugin['rules']>
+}
+
+declare module 'eslint-plugin-import/config/flat/recommended' {
+  import type {
+    Linter,
+  } from 'eslint'
+  const config: Linter.Config
+  export default config
 }
