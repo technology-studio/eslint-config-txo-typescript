4a52c5dce29ecd8a367aead773c4cbbc44fdd98c
Erik Slovak <erik.slovak@technologystudio.sk>
2024-11-18 21:32:50 +0100

    chore: update comments

--- test/Sample.ts
+++ test/Sample.ts
@@ -8,12 +8,12 @@
 
 // VALIDATION SAMPLES
 
-// eslint-disable-next-line import/order, @typescript-eslint/no-unused-vars -- disabled to validate rule
+// eslint-disable-next-line import/order, @typescript-eslint/no-unused-vars -- validation on case when rule should be reported
 import type { importRules } from '../src/configs/import'
-// eslint-disable-next-line import/order, @typescript-eslint/no-unused-vars -- disabled to validate rule
+// eslint-disable-next-line import/order, @typescript-eslint/no-unused-vars -- validation on case when rule should be reported
 import type globals from 'globals'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any -- disabled to validate rule
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- validation on case when rule should be reported
 export const noExplicitAnyError = (...args: any[]): any[] => args
 export const noExplicitAnyOk = (...args: string[]): string[] => args
 
@@ -21,13 +21,13 @@ export const noExplicitAnyOk = (...args: string[]): string[] => args
 export type NoDupeKeysError = { a: number, a: number }
 export type NoDupeKeysOK = { a: number, b: number }
 
-// eslint-disable-next-line no-useless-computed-key -- disabled to validate rule
+// eslint-disable-next-line no-useless-computed-key -- validation on case when rule should be reported
 export const noUselessComputedKeyError = { ['abc']: '1' }
 export const noUselessComputedKeyOk = { abc: '1' }
 
 export const multilineCommaError = {
   a: '1',
-  // eslint-disable-next-line @stylistic/comma-dangle -- disabled to validate rule
+  // eslint-disable-next-line @stylistic/comma-dangle -- validation on case when rule should be reported
   b: '2'
 }
 export const multilineCommaOk = {
