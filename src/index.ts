63ba0c8ba1aa37be2f41fd3cb7d5c72308b52b04
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2026-08-05 09:31:01 +0200

    fix: remove project parser option from jest config

--- src/index.ts
+++ src/index.ts
@@ -68,9 +68,6 @@ export const jestConfig: TSESLint.FlatConfig.Config = {
       ...globals.jest,
     },
     parser,
-    parserOptions: {
-      project: './__tests__/tsconfig.json',
-    },
   },
   settings: {
     'import/resolver': {
